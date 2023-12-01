f8d6b16f3b27e5c561ce6812269c88c8e148c456
linyjj <yj0129999@gmail.com>
2023-12-01 10:58:21 +0800

    week12

---
+++ week12/week12-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        ans=0 #迴圈前面ans=0
+        while n>0: #剝皮法,一次撥一層皮(第07週)
+            ans+=n%2 #撥下來的屑屑,收起來
+            n=n//2 #洋蔥剝皮後,變小了
+        return ans #迴圈後面 ans拿來用
